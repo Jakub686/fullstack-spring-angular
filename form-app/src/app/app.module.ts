e0b8c9573c81737b2714467eeec61e79cdddac5c
Jakub <jakub.bchyla@gmail.com>
2022-12-11 13:13:47 +0100

    addes employee list

--- form-app/src/app/app.module.ts
+++ form-app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { HttpClientModule } from '@angular/common/http'
 import { FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CreateEmployeeComponent } from './create-employee/create-employee.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CreateEmployeeComponent
+    CreateEmployeeComponent,
+    EmployeeListComponent
   ],
   imports: [
     BrowserModule,
